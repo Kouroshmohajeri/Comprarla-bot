5f7c7575c259b9a943024aa89cbea0c4efffd225
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-13 01:21:24 +0200

    logging executable path

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 // import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { exec } from "child_process";
 import telegramRoutes from "./routes/telegramRoutes.js";
 
 dotenv.config();
@@ -20,6 +21,22 @@ app.use(express.json());
 // Import routes
 app.use("/api/products", productRoutes);
 app.use("/api/telegram", telegramRoutes);
+
+exec("which google-chrome", (err, stdout) => {
+  if (err) {
+    console.error("Error finding Google Chrome:", err);
+    return;
+  }
+  console.log("Google Chrome path:", stdout.trim());
+});
+
+exec("which chromium-browser", (err, stdout) => {
+  if (err) {
+    console.error("Error finding Chromium Browser:", err);
+    return;
+  }
+  console.log("Chromium Browser path:", stdout.trim());
+});
 
 const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
