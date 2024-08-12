86b90ee12c80fd2717844a6a085d114cddafe4c0
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-12 14:44:24 +0200

    port fixed

--- client/api/server.js
+++ client/api/server.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    "http://comprarla-bot-server.onrender.com:8800",
+    "http://comprarla-bot-server.onrender.com:8800/api",
 });
 
 export default API;
