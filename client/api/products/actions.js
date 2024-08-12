30cb53586fa80b66393ec98f60b6f8669bc0c474
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-12 13:12:03 +0200

    alert deleted

--- client/api/products/actions.js
+++ client/api/products/actions.js
@@ -3,7 +3,6 @@ import API from "../server.js";
 export const fetchProductData = async (url) => {
   try {
     const response = await API.post("/products/get", { url });
-    alert(response);
     return response.data;
   } catch (error) {
     console.error("Error fetching product data:", error);
