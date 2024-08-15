1e860d4d13279b7ca6ae81c3b010376e36f6a9a1
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-15 14:00:25 +0200

    logo changed

--- client/components/header/header.js
+++ client/components/header/header.js
@@ -1,15 +1,12 @@
 import React from "react";
-import Image from "next/image";
 import styles from "./header.module.css";
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Image
-        src="/logo/comprarlaLogo.png" // Correct path for images in public directory
+      <img
+        src="/logo/comprarlaLogo.svg" // Path to the SVG file
         alt="Comprarla Logo"
-        width={300} // You can adjust the width as needed
-        height={100} // Adjust the height as needed
         className={styles.logo}
       />
     </div>
