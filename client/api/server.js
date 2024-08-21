9171bb2c8af7173c1dc53f1284a8935b6051fa0f
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-21 15:01:04 +0200

    port removed

--- client/api/server.js
+++ client/api/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://comprarla.es:8443/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://comprarla.es/api",
 });
 
 export default API;
