5f2b3ed0ee52d23e52b4e72ada7245422d65f420
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-24 03:17:50 +0200

    port added to test

--- server/src/teleBot.js
+++ server/src/teleBot.js
@@ -27,7 +27,7 @@ app.listen(port, () => {
 });
 
 // Set up the webhook with Telegram
-bot.telegram.setWebhook(`https://localhost${webhookPath}`);
+bot.telegram.setWebhook(`https://localhost:30001${webhookPath}`);
 
 const backendAPIUrl = `${process.env.BACKEND_URL}/api/users`;
 const web_link = "https://comprarla.es/";
