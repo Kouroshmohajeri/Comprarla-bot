e8fb2dcfb3d86126e0e0bb27c14b6f711e212c15
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-25 15:23:12 +0200

    +

--- server/src/controllers/taskDoneController.js
+++ server/src/controllers/taskDoneController.js
@@ -1,27 +1,43 @@
-import TasksDone from "../models/TasksDone.js"; // Import the TasksDone model
-import Task from "../models/Task.js"; // Import the Task model
+import mongoose from "mongoose";
+import TasksDone from "../models/TasksDone.js";
+import Task from "../models/Task.js";
 
-// Mark a task as done
 export const markTaskAsDone = async (req, res) => {
   try {
     const { userId, taskId } = req.body;
 
+    // Check if ObjectId format is valid
+    if (
+      !mongoose.Types.ObjectId.isValid(userId) ||
+      !mongoose.Types.ObjectId.isValid(taskId)
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Invalid userId or taskId format" });
+    }
+
+    const userObjectId = mongoose.Types.ObjectId(userId);
+    const taskObjectId = mongoose.Types.ObjectId(taskId);
+
     // Validate that the task exists
-    const task = await Task.findById(taskId);
+    const task = await Task.findById(taskObjectId);
     if (!task) {
       return res.status(404).json({ message: "Task not found" });
     }
 
     // Check if the task has already been marked as done by this user
-    const existingTaskDone = await TasksDone.findOne({ userId, taskId });
+    const existingTaskDone = await TasksDone.findOne({
+      userId: userObjectId,
+      taskId: taskObjectId,
+    });
     if (existingTaskDone) {
       return res.status(400).json({ message: "Task already marked as done" });
     }
 
     // Create a new TasksDone document
     const taskDone = new TasksDone({
-      userId,
-      taskId,
+      userId: userObjectId,
+      taskId: taskObjectId,
     });
 
     // Save the new taskDone entry and update the task
@@ -31,7 +47,7 @@ export const markTaskAsDone = async (req, res) => {
 
     res.status(201).json({ message: "Task marked as done", taskDone });
   } catch (error) {
-    console.error("Error marking task as done:", error); // Log the error for debugging
+    console.error("Error marking task as done:", error);
     res.status(500).json({ error: error.message });
   }
 };
@@ -40,10 +56,18 @@ export const markTaskAsDone = async (req, res) => {
 export const getTasksDoneByUser = async (req, res) => {
   try {
     const { userId } = req.params;
-    const tasksDone = await TasksDone.find({ userId }).populate("taskId");
 
+    // Check if ObjectId format is valid
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: "Invalid userId format" });
+    }
+
+    const tasksDone = await TasksDone.find({
+      userId: mongoose.Types.ObjectId(userId),
+    }).populate("taskId");
     res.status(200).json(tasksDone);
   } catch (error) {
+    console.error("Error fetching tasks done by user:", error);
     res.status(500).json({ error: error.message });
   }
 };
