40d42e2223bc2ab45c6d1a464bff676ebe3ecbcc
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-30 00:45:58 +0200

    +

--- client/middleware.js
+++ client/middleware.js
@@ -1,48 +1,25 @@
 import { NextResponse } from "next/server";
-
-// Function to verify the token using fetch
-async function verifyTokenWithFetch(authToken) {
-  try {
-    const response = await fetch(
-      `https://comprarla.es/api/otp/login?auth=${authToken}`
-    );
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error verifying token with fetch:", error);
-    throw error;
-  }
-}
+import { verifyToken } from "@/api/OTP/actions.js"; // Import the verification function
 
 export async function middleware(req) {
   const url = new URL(req.url);
-
-  // Extract the auth token from the URL query parameters
   const authToken = url.searchParams.get("auth");
 
   if (authToken) {
     try {
-      // Verify the token by making a request to the backend
-      const data = await verifyTokenWithFetch(authToken);
+      const data = await verifyToken(authToken);
 
       if (data.message === "Token is valid. Proceed with login.") {
-        // Redirect to the dashboard if the token is valid
-        return NextResponse.redirect("/");
+        return NextResponse.redirect(new URL("/dashboard", req.url));
       } else {
-        // Redirect to an error page if the token is invalid
-        return NextResponse.redirect("/error");
+        return NextResponse.redirect(new URL("/error", req.url));
       }
     } catch (error) {
       console.error("Error verifying token:", error);
-      return NextResponse.redirect("/error"); // handle fetch errors
+      return NextResponse.redirect(new URL("/error", req.url));
     }
   }
 
-  // Continue as normal if there's no auth token
+  // Continue to the requested page if no auth token is found
   return NextResponse.next();
 }
