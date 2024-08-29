f8383d42edbefadc8665b9b305f443bd8a3d6e31
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-08-29 23:29:18 +0200

    fresh /start

--- server/src/OTPBot.js
+++ server/src/OTPBot.js
@@ -5,12 +5,21 @@ import connectDB from "./config/db.js";
 // Replace with your actual Telegram bot token
 const token = "7253261804:AAG844nfGGTUEXeMtDxZk2NpSoGIKrZpZlg";
 connectDB();
+
 // Initialize the bot
 const bot = new Telegraf(token);
 
 // Handle /start command
 bot.start(async (ctx) => {
-  await handleStart(ctx);
+  // Check if the start command includes the parameter `auth`
+  if (ctx.startPayload === "auth") {
+    console.log("Starting fresh for user:", ctx.from.id);
+    await handleStart(ctx);
+  } else {
+    // Handle other cases or default behavior if needed
+    console.log("User started bot without 'auth' payload:", ctx.from.id);
+    ctx.reply("Please use the correct link to start the bot.");
+  }
 });
 
 // Launch the bot
