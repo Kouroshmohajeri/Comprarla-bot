668df9f9bff1e8cbdccc4b2b9a01ffc30b979604
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-09-02 00:20:59 +0200

    +

--- server/src/routes/otpRoutes.js
+++ server/src/routes/otpRoutes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import {
   handleStart,
   handleTokenVerification,
-  handleTokenRefresh,
 } from "../controllers/otpController.js";
 
 const router = Router();
@@ -12,8 +11,5 @@ router.get("/start", handleStart);
 
 // Define the /login route for token verification
 router.get("/login", handleTokenVerification);
-
-// Define the /login route for token verification
-router.get("/login", handleTokenRefresh);
 
 export default router;
