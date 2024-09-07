889ba153cc2a2675cb49564212ff867a51b84e50
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-09-07 13:22:15 +0200

    convert logic changed

--- client/lib/converter.js
+++ client/lib/converter.js
@@ -12,20 +12,20 @@ export const convertPrice = (price, fixedPrice) => {
   }
 
   let adjustedPrice = price;
-
+  adjustedPrice += price * 0.25;
   // Apply the percentage adjustment based on the price range
-  if (price <= 110) {
-    adjustedPrice += price * 0.5; // Add 50%
-  } else if (price <= 310) {
-    adjustedPrice += price * 0.4; // Add 40%
-  } else if (price <= 610) {
-    adjustedPrice += price * 0.3; // Add 30%
-  } else if (price <= 1000) {
-    adjustedPrice += price * 0.25; // Add 25%
-  } else {
-    adjustedPrice += price * 0.2; // Add 20%
-  }
+  // if (price <= 110) {
+  //   adjustedPrice += price * 0.5; // Add 50%
+  // } else if (price <= 310) {
+  //   adjustedPrice += price * 0.4; // Add 40%
+  // } else if (price <= 610) {
+  //   adjustedPrice += price * 0.3; // Add 30%
+  // } else if (price <= 1000) {
+  //   adjustedPrice += price * 0.25; // Add 25%
+  // } else {
+  //   adjustedPrice += price * 0.2; // Add 20%
+  // }
 
   // Multiply the adjusted price by the fixed conversion rate
-  return adjustedPrice * fixedPrice + 5 * fixedPrice;
+  return adjustedPrice * fixedPrice + 15 * fixedPrice;
 };
