21ab42be4b2f4a7274f3238831e9d8bd44999b5b
Kourosh Mohajeri <kouroshmohajeriii8@gmail.com>
2024-09-12 01:32:54 +0200

    type changed

--- server/src/models/Session.js
+++ server/src/models/Session.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const sessionSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: String, ref: "User", required: true },
   sessionToken: { type: String, required: true },
   expiresAt: { type: Date, required: true }, // Expiration time for the session
   createdAt: { type: Date, default: Date.now },
